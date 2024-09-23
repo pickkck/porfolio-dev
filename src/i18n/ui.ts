8e203df3f4509006edae5de5880e9fb63abec3bf
Aaron Ramirez <124172831+AaronRmzV@users.noreply.github.com>
2024-09-23 16:08:30 -0600

    Add i18n english and spanish to all project

---
+++ src/i18n/ui.ts
@@ -0,0 +1,14 @@
+import es from './es.json';
+import en from './en.json';
+
+export const languages = {
+  es: 'Español 🇲🇽',
+  en: 'English 🇬🇧',
+};
+export const showDefaultLang = false;
+export const defaultLang = 'es';
+
+export const ui = {
+  es: es,
+  en: en,
+} as const;
